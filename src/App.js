412fd226f1bf5c373be5615286ce2a7f819523c6
DahliaL <dahlia.lapom@gmail.com>
2021-09-25 16:20:28 -0400

    home and start

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'; 
 import './App.css';
 import Header from './Header';
 import HomePage from './HomePage';
+import Start from './Start';
 
 function App() {
+  const [theme, setTheme] = useState('day');
+  const time = new Date().getHours();
+
   return (
     //BEM class naming convention.
     <div className="App">
-    <Header />
-    <HomePage />
-
+     <Router> 
+       <Header/>
+       <Switch>
+    <Route path="/" exact>
+            {' '}
+            <HomePage />{' '}
+          </Route>
+    <Route path="/start" exact>
+            {' '}
+            <Start />{' '}
+          </Route>
+    </Switch>
+    </Router>
     {/* Header with Home buttons, etc.. */}
     {/* Main greeting text varies by TOD*/}
     {/* Start button */}
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import { React, useState } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import styled from 'styled-components';
+import nav from "./nav2.jpeg";
 
-function Header() {
-    return (
-        <div className="head"> 
-            <h1>This is the header.</h1>
-        </div>
-    )
+const NavMenuStyle = styled.div`
+  .header {
+    position: fixed;
+    display: flex;
+  z-index: 100;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 1rem 0;
 }
 
- export default Header
+.header::before {
+    
+}
+
+.nav-logo {
+    height: 99px;
+    width: 170px;
+    margin-left: 15%;
+    align-items: left;
+}
+
+.nav-items {
+    display: flex;
+    margin-left: auto;
+    margin-right: 5%;
+    align-items: center;
+    opacity: 100%;
+}
+
+.nav-link {
+    background-color: #d1d1d1;
+    color: black;
+    padding: 20px 25px;
+    text-align: center;
+    text-decoration: none;
+    border: none;
+    border-radius: 60px;
+    opacity: 80%;
+}
+
+.nav-link:hover {
+    background-color: #A9A9A9;
+    color: black;
+}
+`;
+
+export default function Header(props) {
+    return (
+    <NavMenuStyle> 
+    <div className = "header">
+    <div className="justbg"></div>
+    {/* Logo */}
+    <Link className = "nav-title" to="/landing">
+        <img className = "nav-logo" src={ "/logo.png" } alt="Adventours logo" />
+    </Link>
+
+    {/* Page Links */}
+    <div className = "nav-items">
+        <Link className = "nav-link" to='/'>Home</Link>
+        <Link className = "nav-link" to='/prev'>Previous Adventours</Link>
+        <Link className = "nav-link" to='/friends'>Friends</Link>
+    </div>
+</div>
+</NavMenuStyle> 
+)
+};
