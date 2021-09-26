7c9abf99c3e4136cd4b736d789c10cf98a0bd0d8
Wanyea <61568218+Wanyea@users.noreply.github.com>
2021-09-26 01:45:27 -0400

    Third Commit

--- src/SimpleMap.js
+++ src/SimpleMap.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import Geocode from "react-geocode";
 import GetLocation from './GetLocation';
 
 const SimpleMap = () => {
-  
   const location = GetLocation().coordinates;
-  console.log(location)
+
+  Geocode.setApiKey("AIzaSyBhMbvP6pFgIOQ1IdsvqoYBxFbA4W-Ir2k")
+  Geocode.setLanguage("en")
+  Geocode.fromLatLng(location.lat.toString(), location.lng.toString()).then(
+    (response) => {
+      const address = response.results[0].formatted_address
+      console.log(address)
+    },
+    (error) => {
+      console.log(error)
+    }
+  )
   const mapStyles = {        
     height: "100vh",
     width: "100%"};
@@ -20,7 +31,7 @@ const SimpleMap = () => {
        googleMapsApiKey='AIzaSyBhMbvP6pFgIOQ1IdsvqoYBxFbA4W-Ir2k'>
         <GoogleMap
           mapContainerStyle={mapStyles}
-          zoom={13}
+          zoom={16}
           center={defaultCenter}
         >
           <Marker position={{ lat: location.lat, lng: location.lng }} />
